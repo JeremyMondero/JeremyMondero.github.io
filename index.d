158155a28d773cfabe315e52cc4608d95710bcad
Jeremy Mondero <mond316@csu.fullerton.edu>
2025-05-07 12:25:53 -0700

    Final Draft

---
+++ index.d
@@ -0,0 +1,16 @@
+
+# Jeremy Mondero's Portfolio 
+
+I am a computer engineering major at CSUF and plan on graduating 2028
+
+## Favortie CPSC 120L Labs
+
+* Lab 4
+ part 1 was a favorite of mine because I liked how we had to use math to find out the different values for ml, tbsp, and oz and was able to learn to use doubles.
+
+
+* Lab 9
+ part 1 was another one of my favorite labs because we had to implement what we learned on vectors and also booleans. I also liked how challenging the code was.
+
+* Lab 7
+ part 1 was my third favorite lab because I enjoyed having to work with if statements and having to figure out the different times in which people are allowed to park. 
